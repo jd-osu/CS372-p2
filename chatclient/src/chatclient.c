3efa9937322da21d3f77f2f4c0176f9f74a02849
jd-osu <dimedioj@oregonstate.edu>
2018-10-27 20:50:48 -0400

    sample socket exchange

--- chatclient/src/chatclient.c
+++ chatclient/src/chatclient.c
@@ -9,6 +9,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+/************************************************
+ * NAME
+ *   error
+ * DESCRIPTION
+ *   This program takes as an argument a string
+ *   representing an error message and an int
+ *   representing an exit conditon and outputs
+ *   the message to stderr and exits.
+ * *********************************************/
+void error(const char *msg, int e)
+{
+  fprintf(stderr,"%s\n", msg);
+  exit(e);
+}
 
 /************************************************
  * NAME
@@ -28,11 +45,7 @@ void get_handle(char* handle)
   // allocate memory for input string
   input = (char *)malloc(input_size * sizeof(char));
   if (input == NULL)
-  {
-    fprintf(stderr, "Unable to allocate");
-    perror("Error in get_choice");
-    exit(1);
-  }
+	  error("Unable to allocate (get_handle)", 1);
 
   // until a valid input is received...
   while (valid_input == 0)
@@ -76,6 +89,51 @@ int main(int argc, char **argv)
   printf("Handle is: %s\n", handle);
   printf("Hostname is: %s\n", hostname);
   printf("Port is: %d\n", port);
+
+  /* The following code was adapted from CS344, Lecture 4.2, slide 21
+   * "client.c"
+   */
+  int socketFD, charsWritten, charsRead;
+
+  struct sockaddr_in serverAddress;
+  struct hostent* serverHostInfo;
+  char buffer[256];
+
+  memset((char*)&serverAddress, '\0', sizeof(serverAddress));
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(port);
+  serverHostInfo = gethostbyname(hostname);
+
+  if (serverHostInfo == NULL) error("ERROR, no such host",1);
+
+  memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
+
+  //set up the socket
+  socketFD = socket(AF_INET, SOCK_STREAM, 0);
+  if (socketFD < 0) error("ERROR opening socket", 1);
+
+  //connect to server
+  if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0)
+	  error("ERROR connecting", 1);
+
+  //get input message from user
+  printf("Enter text to send to the server: ");
+  memset(buffer, '\0', sizeof(buffer));
+  fgets(buffer, sizeof(buffer) - 1, stdin);
+  buffer[strcspn(buffer, "\n")] = '\0';
+
+  // send message to server
+  charsWritten = send(socketFD, buffer, strlen(buffer), 0);
+  if (charsWritten < 0) error("ERROR writing to socket", 1);
+  if (charsWritten < strlen(buffer)) printf("WARNING: Not all data written to socket.\n");
+
+  //get return message from server
+  memset(buffer, '\0', sizeof(buffer));
+  charsRead = recv(socketFD, buffer, sizeof(buffer)-1, 0);
+  if (charsRead < 0) error ("ERROR reading from socket", 1);
+  printf("Received from server: \"%s\"\n", buffer);
+
+  close(socketFD);
 
   return EXIT_SUCCESS;
 }
--- chatserve/chatserve.py
+++ chatserve/chatserve.py
@@ -6,6 +6,8 @@
 # This program ...
 
 import sys
+import socket
+from _socket import SOCK_STREAM, AF_INET
 
 if (len(sys.argv) < 2) : 
     print "USAGE: ", str(sys.argv[0]), " port"
@@ -13,6 +15,19 @@ if (len(sys.argv) < 2) :
 
 port = int(sys.argv[1])
 
-print "port: ", str(port)
 
-print "test"
+# The following code has been adapted from CS372, Lecture 15, slide 9
+# "Example application: TCP server"
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('',port))
+serverSocket.listen(1)
+
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+    
+    sentence = connectionSocket.recv(1024)
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence)
+    connectionSocket.close()
+
+
