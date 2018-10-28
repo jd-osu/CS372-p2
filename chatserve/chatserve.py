9484afb5d561041a523b9c8fbf20289ca7b2a4f6
jd-osu <dimedioj@oregonstate.edu>
2018-10-27 23:08:41 -0400

    exp

--- chatserve/chatserve.py
+++ chatserve/chatserve.py
@@ -28,7 +28,7 @@ while 1:
 
     in_msg_length = 1
     
-    while in_msg_length != 0 && out_msg_length != 0 :
+    while in_msg_length != 0 and out_msg_length != 0 :
         in_msg = connectionSocket.recv(1024)
         in_msg_length = len(in_msg)
         
