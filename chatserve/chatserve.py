e0d2df68250ebc2def4822ae601ab4fa7df77a5e
jd-osu <dimedioj@oregonstate.edu>
2018-10-27 20:11:18 -0400

    server command line input

--- chatserve/chatserve.py
+++ chatserve/chatserve.py
@@ -1,1 +1,16 @@
-print("test")
+# !/usr/bin/python
+# mypython
+# Author: 
+# CS344
+# February 24, 2018
+# This program ...
+
+import sys
+
+if (len(sys.argv) < 2) : 
+    print('USAGE: ', str(sys.argv[0]), ' port')
+    exit(1)
+
+port = int(sys.argv[1])
+
+print("port: ", str(port))
