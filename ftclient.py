428730928b6dddd186e7eeaceb4c7149acb16b9e
jd-osu <dimedioj@oregonstate.edu>
2018-11-23 11:16:16 -0500

    input

--- ftclient.py
+++ ftclient.py
@@ -29,7 +29,7 @@ else :
 if (not(server_port.isdigit) or
     not(data_port.isdigit) or
     server_port < 1024 or
-    server_port > 1024 or
+    server_port > 65535 or
     data_port < 1024 or
     data_port > 65535) :
     print "Invalid port. Port must be integer in range: 1024-65535"
